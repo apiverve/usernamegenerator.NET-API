fa186066ea00079725560af2a387ea951531885a
APIVerve Team <hello@apiverve.com>
2024-05-14 15:44:42 -0500

    Bug fixes and Improvements

---
+++ usernamegeneratorQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class usernamegeneratorQueryOptions {
+public int count { get; set; }
+public string email { get; set; }
+}
+
+}
